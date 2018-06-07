65a51738d91e66b24ac1d518109ec0a328103a47
juanchitox91 <37164311+juanchitox91@users.noreply.github.com>
2018-06-07 05:41:50 -0400

    proyecto hasta la clase 91

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders } from './app.routing';
+
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './components/home.component';
+import { UserEditComponent } from './components/user-edit.component';
+import { ArtistListComponent } from './components/artist-list.component';
+import { ArtistAddComponent } from './components/artist-add.component';
+import { ArtistEditComponent } from './components/artist-edit.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserEditComponent,
+    HomeComponent,
+    ArtistListComponent,
+    ArtistAddComponent,
+    ArtistEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+
+  ],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
